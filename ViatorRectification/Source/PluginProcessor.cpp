408dd706866e4650307c861f8989f55a2fbbdc04
Landon <33662627+landonviator@users.noreply.github.com>
2021-01-05 23:48:26 -0600

    Made the rectification and hard clipper

---
+++ ViatorRectification/Source/PluginProcessor.cpp
@@ -0,0 +1,193 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+ViatorRectificationAudioProcessor::ViatorRectificationAudioProcessor()
+#ifndef JucePlugin_PreferredChannelConfigurations
+     : AudioProcessor (BusesProperties()
+                     #if ! JucePlugin_IsMidiEffect
+                      #if ! JucePlugin_IsSynth
+                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
+                      #endif
+                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
+                     #endif
+                       )
+#endif
+{
+}
+
+ViatorRectificationAudioProcessor::~ViatorRectificationAudioProcessor()
+{
+}
+
+//==============================================================================
+const juce::String ViatorRectificationAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool ViatorRectificationAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool ViatorRectificationAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool ViatorRectificationAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double ViatorRectificationAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int ViatorRectificationAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int ViatorRectificationAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void ViatorRectificationAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const juce::String ViatorRectificationAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void ViatorRectificationAudioProcessor::changeProgramName (int index, const juce::String& newName)
+{
+}
+
+//==============================================================================
+void ViatorRectificationAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    // Use this method as the place to do any pre-playback
+    // initialisation that you need..
+}
+
+void ViatorRectificationAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+}
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool ViatorRectificationAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    juce::ignoreUnused (layouts);
+    return true;
+  #else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+        return false;
+
+    // This checks if the input layout matches the output layout
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+#endif
+
+void ViatorRectificationAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+{
+    juce::ScopedNoDenormals noDenormals;
+    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumOutputChannels = getTotalNumOutputChannels();
+
+    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
+
+    for (int channel = 0; channel < totalNumInputChannels; ++channel)
+    {
+        auto* outputData = buffer.getWritePointer (channel);
+        auto* inputData = buffer.getReadPointer(channel);
+
+        for (auto sample = 0; sample < buffer.getNumSamples(); sample++) {
+            
+//            if (inputData[sample] >= 0) {
+//                halfWaveRectification = inputData[sample];
+//            } else {
+//                halfWaveRectification = 0;
+//            }
+            auto input = inputData[sample] * .1;
+            
+            if (input >= 0) {
+                fullWaveRectification = input;
+            } else {
+                fullWaveRectification = input * -1;
+            }
+            outputData[sample] = fullWaveRectification;
+        }
+    }
+}
+
+//==============================================================================
+bool ViatorRectificationAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+juce::AudioProcessorEditor* ViatorRectificationAudioProcessor::createEditor()
+{
+    return new ViatorRectificationAudioProcessorEditor (*this);
+}
+
+//==============================================================================
+void ViatorRectificationAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+}
+
+void ViatorRectificationAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new ViatorRectificationAudioProcessor();
+}
