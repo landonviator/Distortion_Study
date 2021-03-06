61fa0c7126adceaf2e9443c5e4d5f79dc437f35f
Landon <33662627+landonviator@users.noreply.github.com>
2021-03-05 19:33:16 -0600

    added pedal model

---
+++ pedal_iR_Prototyper/Source/PluginEditor.cpp
@@ -0,0 +1,106 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+Pedal_iR_PrototyperAudioProcessorEditor::Pedal_iR_PrototyperAudioProcessorEditor (Pedal_iR_PrototyperAudioProcessor& p)
+    : AudioProcessorEditor (&p), audioProcessor (p)
+{
+    shadowProperties.radius = 24;
+    shadowProperties.offset = juce::Point<int> (-1, 3);
+    dialShadow.setShadowProperties (shadowProperties);
+                
+    //Input slider
+    addAndMakeVisible(inputSlider);
+    inputSlider.setSliderStyle(juce::Slider::SliderStyle::RotaryVerticalDrag);
+    inputSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 64, 32);
+    inputSlider.setTextValueSuffix(" dB");
+    inputSlider.setColour(0x1001400, juce::Colour::fromFloatRGBA(1, 1, 1, 0.0f));
+    inputSlider.setColour(0x1001700, juce::Colour::fromFloatRGBA(1, 1, 1, 0.0f));
+    inputSlider.setColour(0x1001500, juce::Colour::fromFloatRGBA(0, 0, 0, 0.0f));
+    inputSlider.setLookAndFeel(&customDial);
+    inputSlider.setComponentEffect(&dialShadow);
+    inputSliderAttach = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.treeState, inputSliderId, inputSlider);
+    inputSlider.setRange(-24.0, 24.0, 0.25);
+    
+    addAndMakeVisible(inputLabel);
+    inputLabel.setText("Input", juce::dontSendNotification);
+    inputLabel.setJustificationType(juce::Justification::centred);
+    inputLabel.setColour(0x1000281, juce::Colour::fromFloatRGBA(1, 1, 1, 0.25f));
+    inputLabel.attachToComponent(&inputSlider, false);
+                
+    //Trim slider
+    addAndMakeVisible(trimSlider);
+    trimSlider.setSliderStyle(juce::Slider::SliderStyle::RotaryVerticalDrag);
+    trimSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 72, 32);
+    trimSlider.setTextValueSuffix(" dB");
+    trimSlider.setColour(0x1001400, juce::Colour::fromFloatRGBA(1, 1, 1, 0.0f));
+    trimSlider.setColour(0x1001700, juce::Colour::fromFloatRGBA(1, 1, 1, 0.0f));
+    trimSlider.setColour(0x1001500, juce::Colour::fromFloatRGBA(0, 0, 0, 0.0f));
+    trimSlider.setLookAndFeel(&customDial);
+    trimSlider.setComponentEffect(&dialShadow);
+    trimSliderAttach = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.treeState, trimSliderId, trimSlider);
+    trimSlider.setRange(-24.0, 24.0, 0.25);
+    
+    addAndMakeVisible(trimLabel);
+    trimLabel.setText("Trim", juce::dontSendNotification);
+    trimLabel.setJustificationType(juce::Justification::centred);
+    trimLabel.setColour(0x1000281, juce::Colour::fromFloatRGBA(1, 1, 1, 0.25f));
+    trimLabel.attachToComponent(&trimSlider, false);
+    
+    setSize (400, 300);
+}
+
+Pedal_iR_PrototyperAudioProcessorEditor::~Pedal_iR_PrototyperAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void Pedal_iR_PrototyperAudioProcessorEditor::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    juce::Rectangle<int> background = AudioProcessorEditor::getLocalBounds();
+        g.setGradientFill(juce::ColourGradient::vertical(juce::Colour::fromFloatRGBA(0.18f, 0.20f, 0.24f, 1.0), 300 * .01, juce::Colour::fromFloatRGBA(0.12f, 0.14f, 0.18f, 1.0), 300 * .99));
+        g.fillRect(background);
+        g.setColour (juce::Colours::white.darker(1.0));
+        g.setFont (36.0f);
+        g.drawFittedText ("Pedal Simulator", 0, 24, AudioProcessorEditor::getWidth(), AudioProcessorEditor::getHeight(), juce::Justification::centredTop, 1);
+}
+
+void Pedal_iR_PrototyperAudioProcessorEditor::resized()
+{
+    //Master bounds object
+    juce::Rectangle<int> bounds = getLocalBounds();
+                        
+    //first column of gui
+    juce::FlexBox flexboxColumnOne;
+    flexboxColumnOne.flexDirection = juce::FlexBox::Direction::column;
+    flexboxColumnOne.flexWrap = juce::FlexBox::Wrap::noWrap;
+    flexboxColumnOne.alignContent = juce::FlexBox::AlignContent::stretch;
+                       
+    juce::Array<juce::FlexItem> itemArrayColumnOne;
+    itemArrayColumnOne.add(juce::FlexItem(bounds.getWidth() / 4, bounds.getHeight() / 2, inputSlider).withMargin(juce::FlexItem::Margin(bounds.getHeight() * .5, 0, 0, 0)));
+                       
+    flexboxColumnOne.items = itemArrayColumnOne;
+    flexboxColumnOne.performLayout(bounds.removeFromLeft(bounds.getWidth() / 2));
+    /* ============================================================================ */
+                   
+    //second column of gui
+    juce::FlexBox flexboxColumnTwo;
+    flexboxColumnTwo.flexDirection = juce::FlexBox::Direction::column;
+    flexboxColumnTwo.flexWrap = juce::FlexBox::Wrap::noWrap;
+    flexboxColumnTwo.alignContent = juce::FlexBox::AlignContent::stretch;
+                       
+    juce::Array<juce::FlexItem> itemArrayColumnTwo;
+    itemArrayColumnTwo.add(juce::FlexItem(bounds.getWidth() / 4, bounds.getHeight() / 2, trimSlider).withMargin(juce::FlexItem::Margin(bounds.getHeight() * .5, 0, 0, 0)));
+                       
+    flexboxColumnTwo.items = itemArrayColumnTwo;
+    flexboxColumnTwo.performLayout(bounds.removeFromLeft(bounds.getWidth()));
+}
---
+++ pedal_iR_Prototyper/Source/PluginProcessor.cpp
@@ -0,0 +1,227 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+Pedal_iR_PrototyperAudioProcessor::Pedal_iR_PrototyperAudioProcessor()
+#ifndef JucePlugin_PreferredChannelConfigurations
+     : AudioProcessor (BusesProperties()
+                     #if ! JucePlugin_IsMidiEffect
+                      #if ! JucePlugin_IsSynth
+                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
+                      #endif
+                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
+                     #endif
+                       ),
+treeState (*this, nullptr, "PARAMETER", createParameterLayout())
+#endif
+{
+}
+
+Pedal_iR_PrototyperAudioProcessor::~Pedal_iR_PrototyperAudioProcessor()
+{
+}
+
+//==============================================================================
+const juce::String Pedal_iR_PrototyperAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool Pedal_iR_PrototyperAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool Pedal_iR_PrototyperAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool Pedal_iR_PrototyperAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double Pedal_iR_PrototyperAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int Pedal_iR_PrototyperAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int Pedal_iR_PrototyperAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void Pedal_iR_PrototyperAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const juce::String Pedal_iR_PrototyperAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void Pedal_iR_PrototyperAudioProcessor::changeProgramName (int index, const juce::String& newName)
+{
+}
+
+//==============================================================================
+void Pedal_iR_PrototyperAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    juce::dsp::ProcessSpec spec;
+    spec.maximumBlockSize = samplesPerBlock;
+    spec.sampleRate = sampleRate;
+    spec.numChannels = getTotalNumOutputChannels();
+    
+    inputProcessor.prepare(spec);
+    inputProcessor.reset();
+    
+    convolutionProcessor.prepare(spec);
+    convolutionProcessor.reset();
+    
+    trimProcessor.prepare(spec);
+    trimProcessor.reset();
+            
+    convolutionProcessor.loadImpulseResponse
+        (BinaryData::highGainLowDrive_wav,
+         BinaryData::highGainLowDrive_wavSize,
+         juce::dsp::Convolution::Stereo::yes,
+         juce::dsp::Convolution::Trim::yes, 0,
+         juce::dsp::Convolution::Normalise::yes);
+}
+
+void Pedal_iR_PrototyperAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+}
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool Pedal_iR_PrototyperAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    juce::ignoreUnused (layouts);
+    return true;
+  #else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    // Some plugin hosts, such as certain GarageBand versions, will only
+    // load plugins that support stereo bus layouts.
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+        return false;
+
+    // This checks if the input layout matches the output layout
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+#endif
+
+void Pedal_iR_PrototyperAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+{
+    juce::ScopedNoDenormals noDenormals;
+    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumOutputChannels = getTotalNumOutputChannels();
+    
+    auto* rawInput = treeState.getRawParameterValue(inputSliderId);
+    auto* rawTrim = treeState.getRawParameterValue(trimSliderId);
+
+
+    juce::dsp::AudioBlock<float> audioBlock {buffer};
+    
+    convolutionProcessor.process(juce::dsp::ProcessContextReplacing<float> (audioBlock));
+    
+    inputProcessor.setGainDecibels(*rawInput);
+    inputProcessor.process(juce::dsp::ProcessContextReplacing<float> (audioBlock));
+    
+    for (int channel = 0; channel < totalNumInputChannels; ++channel)
+    {
+        auto* inputData = buffer.getReadPointer (channel);
+        auto* outputData = buffer.getWritePointer (channel);
+        
+        for (int sample = 0; sample < buffer.getNumSamples(); sample++) {
+            outputData[sample] = 2/3.14 * atan(inputData[sample] * 12.0);
+        }
+    }
+    
+    
+    trimProcessor.setGainDecibels(*rawTrim);
+    trimProcessor.process(juce::dsp::ProcessContextReplacing<float> (audioBlock));
+}
+
+//==============================================================================
+bool Pedal_iR_PrototyperAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+juce::AudioProcessorEditor* Pedal_iR_PrototyperAudioProcessor::createEditor()
+{
+    return new Pedal_iR_PrototyperAudioProcessorEditor (*this);
+}
+
+//==============================================================================
+void Pedal_iR_PrototyperAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+}
+
+void Pedal_iR_PrototyperAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+}
+
+juce::AudioProcessorValueTreeState::ParameterLayout Pedal_iR_PrototyperAudioProcessor::createParameterLayout()
+{
+    std::vector <std::unique_ptr<juce::RangedAudioParameter>> params;
+    params.reserve(2);
+    
+    auto inputParam = std::make_unique<juce::AudioParameterFloat>(inputSliderId, inputSliderName, -24.0f, 24.0f, 0.0f);
+    auto trimParam = std::make_unique<juce::AudioParameterFloat>(trimSliderId, trimSliderName, -24.0f, 24.0f, 0.0f);
+
+    params.push_back(std::move(inputParam));
+    params.push_back(std::move(trimParam));
+    
+    return { params.begin(), params.end() };
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new Pedal_iR_PrototyperAudioProcessor();
+}
